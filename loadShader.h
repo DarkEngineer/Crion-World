6dc65ec9c59da0a40964fc1d527df760649a4d7f
DarkEngineer <maniodark@gmail.com>
2013-01-22 08:51:40 -0800

    header

---
+++ loadShader.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <glm\glm.hpp>
+#include <GL\glew.h>
+#include <GL\glut.h>
+#include <GL\GL.h>
+#include <GL\GLU.h>
+#include <GL\glfw.h>
+
+using namespace std;
+
+string loadShaderFile(const string fileName);
