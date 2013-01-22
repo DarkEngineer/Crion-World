a5a7c315bc671b52298f2db5f9a7c708a57aa66f
Dark Engineer <maniodark@gmail.com>
2013-01-23 00:47:32 +0100

    shader check functions

--- loadShader.cpp
+++ loadShader.cpp
@@ -2,7 +2,7 @@
 
 string loadShaderFile(const string fileName)
 {
-  
+	
 	ifstream shaderFile( fileName.c_str());
 
 	//find the file size
@@ -16,4 +16,46 @@ string loadShaderFile(const string fileName)
 
 	//return the shader string
 	return string( buffer.begin(), buffer.end());
+}
+
+void getShaderLogInfo( GLuint shader )
+{
+	GLint status;
+	glGetShaderiv( shader, GL_COMPILE_STATUS, & status );
+	if( status == GL_TRUE )
+		cout << "SHADER SUCCESSFULLY LOADED" << endl;
+	else
+	{
+		cout << "SHADER DIDN'T LOAD" << endl;
+		std::fstream logs;
+		logs.open( "logs.txt", ios::in | ios::out | ios::trunc );
+		char buffer[ 512 ] ;
+		glGetShaderInfoLog( shader, 512, NULL, buffer );
+		logs << buffer;
+		logs.close();
+		glDeleteShader( shader );
+	}
+
+	return;
+}
+
+void getProgramLogInfo( GLuint program )
+{
+	GLint status;
+	glGetProgramiv( program, GL_LINK_STATUS, & status );
+	if( status == GL_TRUE )
+		cout << "PROGRAM SUCCESSFULLY LOADED" << endl;
+	else
+	{
+		cout << "PROGRAM DIDN'T LOAD" << endl;
+		std::fstream logs;
+		logs.open( "logs.txt", ios::in | ios::out | ios::trunc );
+		char buffer[ 512 ] ;
+		glGetProgramInfoLog( program, 512, NULL, buffer );
+		logs << buffer;
+		logs.close();
+		glDeleteProgram( program );
+	}
+
+	return;
 }
