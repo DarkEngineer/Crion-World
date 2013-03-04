55852cc8ef3a05ba2575d9e92c7b200310198458
Dark Engineer <maniodark@gmail.com>
2013-03-04 16:39:32 +0100

    spliting header on multiple files

--- game.h
+++ game.h
@@ -1,89 +1,27 @@
 #ifndef game_h
 #define game_h
 #include "includes.h"
-class Object;
-class GameIntro;
-class GameMenu;
-class GameMain;
-class GameOptions;
-class GameHighscores;
-class Game;
-
-class Object
-{
-public:
-		const char* image;
-		float x;
-		float y;
-		float width;
-		float height;
-		GLuint VertexBuffer;
-		GLuint vertexShader;
-		GLuint fragmentShader;
-	Object();
-	Object( float x, float y, float width, float height );
-};
+#include "Object.h"
 
 class Game
 {
 public:
+	double i;
+	GLuint vbo;
+	GLuint * crionProgram;
+	GLsizeiptr maxObjects;
+	std::vector<Object> listOfObjects;
 	virtual void render() =0;
+	virtual void reserveSpace() =0;
 	virtual void Update() =0;
 	virtual void setWindow(int width, int height) =0;
 	virtual Game * nextGameState() =0;
+	virtual void makeBuffer();
+	virtual void makeProgram(GLuint program);
+	Game();
 	virtual ~Game();
 	
 };
 
-class GameIntro : public Game
-{
 
-	void loadObjects( std::vector<Object> modelObject);
-	
-	Game * nextGameState();
-public:
-		void setWindow(int width, int height);
-		void render();
-		void Update();
-		~GameIntro();
-};
-
-class GameMenu : public Game
-{
-	void setWindow(int width, int height);
-	void loadObjects( std::vector<Object> modelObject);
-	void render();
-	void Update();
-	Game * nextGameState();
-
-};
-
-class GameOptions : public Game
-{
-	void setWindow(int width, int height);
-	void changeWindowSize( int width, int height );
-	void loadObjects( std::vector<Object> modelObject);
-	void render();
-	void Update();
-	Game * nextGameState();
-};
-
-class GameMain : public Game
-{
-	void setWindow(int width, int height);
-	void loadObjects( std::vector<Object> modelObject);
-	void render();
-	void Update();
-	Game * nextGameState();
-};
-
-class GameHighscores : public Game
-{
-	void setWindow(int width, int height);
-	void loadObjects( std::vector<Object> modelObject);
-	void loadHighscores();
-	void render();
-	void Update();
-	Game * nextGameState();
-};
 #endif
