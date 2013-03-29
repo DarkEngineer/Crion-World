225d237b38b7ae6554a5dfdaabb1cdac65cb2192
Dark Engineer <maniodark@gmail.com>
2013-03-29 20:12:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "game.h"
-#include "loadShader.h"
+#include "Mesh.h"
+#include "Shader.h"
 #include "GameIntro.h"
-#include "loadImage.h"
+#include "glFreeImage.h"
 #include <SOIL\SOIL.h>
 #include <time.h>
 #include <glm\glm.hpp>
@@ -10,8 +11,10 @@
 
 int main( )
 {
+	float width = 800.0f;
+	float height = 600.0f;
 	Game * game = new GameIntro();
-	game->setWindow(800, 600);
+	game->setWindow(static_cast<int>(width), static_cast<int>(height));
 	
 	if (glewInit() != GLEW_OK) 
 	{
@@ -20,115 +23,15 @@ int main( )
 	}
 	glfwSetWindowTitle("Crion World Alpha");
 	
-	
-	game->makeBuffer();
-	game->reserveSpace();
-
-
-	GLuint vao;
-	glGenVertexArrays(1, & vao);
-	glBindVertexArray( vao );
-
-	GLuint vbo;
-	glGenBuffers( 1, &vbo); //generate 1 buffer
-	
-	GLuint ebo;
-	glGenBuffers(1, &ebo);
-
-
-
-	float vertices[] = {
-		//Positiom    Color				Texture coords
-		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,//top-left
-		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,// top-right
-		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,// bottom-right
-		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f// bottom-left
-	};
-
-	GLuint elements[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-	glBindBuffer( GL_ARRAY_BUFFER, vbo );
-	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
-	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ebo );
-	glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( elements ), elements, GL_STATIC_DRAW );
-	
-	// vertex shader loading
-	const string vShaderString = loadShaderFile("SimpleVertexShader.vertexshader").c_str();
-	const GLchar * tempVShader = vShaderString.c_str();
-	GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
-	glShaderSource(vertexShader, 1, & tempVShader, NULL);
-	glCompileShader( vertexShader ); 
-	getShaderLogInfo( vertexShader );
-
-	//fragment shader loading
-	const string fShaderString = loadShaderFile("SimpleFragmentShader.fragmentshader").c_str();
-	const GLchar * tempFShader = fShaderString.c_str();
-	GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-	glShaderSource(fragmentShader, 1, & tempFShader, NULL );
-	glCompileShader(fragmentShader);
-	getShaderLogInfo( fragmentShader );
-
-
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader( shaderProgram, vertexShader );
-	glAttachShader( shaderProgram, fragmentShader);
-	glBindFragDataLocation( shaderProgram, 0, "outColor" );
-	glLinkProgram( shaderProgram );
-	glUseProgram( shaderProgram );
-	getProgramLogInfo( shaderProgram );
-
-	GLint posAttrib = glGetAttribLocation( shaderProgram, "position" );
-	glEnableVertexAttribArray( posAttrib );
-	glVertexAttribPointer( posAttrib, 2, GL_FLOAT, GL_FALSE, 7*sizeof(float), 0 );
-	
-	GLint colAttrib =  glGetAttribLocation( shaderProgram, "color" );
-	glEnableVertexAttribArray( colAttrib );
-	glVertexAttribPointer( colAttrib, 3, GL_FLOAT, GL_FALSE, 7*sizeof(float), (void*)( 2*sizeof(float) ) );
-	GLint texAttrib = glGetAttribLocation( shaderProgram, "texcoord" );
-	glEnableVertexAttribArray( texAttrib );
-	glVertexAttribPointer( texAttrib, 2, GL_FLOAT, GL_FALSE, 7*sizeof(float), (void*)( 5*sizeof(float) ) );
-
-	glFITexture Texture;
-	GLuint textures[ 2 ];
-	glGenTextures( 2, textures );
-
-	glActiveTexture( GL_TEXTURE0 );
-	glBindTexture( GL_TEXTURE_2D, textures[ 0 ] );
-	Texture.Load( "images\\sample.png" );
-	Texture.Bind();
-	glUniform1i( glGetUniformLocation( shaderProgram, "texKitten"), 0 );
-
-	
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-
-
-	glActiveTexture( GL_TEXTURE1 );
-	glBindTexture( GL_TEXTURE_2D, textures[ 1 ] );
-	Texture.Load( "images\\sample2.png" );
-	Texture.Bind();
-	glUniform1i( glGetUniformLocation( shaderProgram, "texPuppy" ), 1 );
-
-	
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-
-
 	glfwEnable( GLFW_STICKY_KEYS );
+	glEnable(GL_DEPTH_TEST);
  
 	while( glfwGetWindowParam( GLFW_OPENED ) )
 	{
 		 game->render();
 		 game = game->nextGameState();
 		
-		glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
     // Swap buffers
 		glfwSwapBuffers();
 
@@ -137,17 +40,6 @@ int main( )
  			break;
  
 	} // Check if the ESC key was pressed or the window was closed
-	
-	
-	glDeleteTextures( 2, textures );
-	glDeleteProgram( shaderProgram );
-	glDeleteShader( vertexShader );
-	glDeleteShader( fragmentShader );
-
-	glDeleteBuffers(1, & ebo);
-	glDeleteBuffers(1,  & vbo );
-	glDeleteVertexArrays(1, &vao );
-
 	
 	
 	glfwTerminate();
