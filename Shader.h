dbbf017e99bafb99623705e53cb5ee511d6d10cd
Dark Engineer <maniodark@gmail.com>
2013-03-30 11:26:07 +0100

    Update and rename loadShader.h to Shader.h

---
+++ Shader.h
@@ -0,0 +1,33 @@
+#ifndef Shader_h
+#define Shader_h
+
+#include "includes.h"
+
+
+class Shader
+{
+  std::string source;
+	GLuint shader;
+	
+	
+public:
+	// shader types
+	enum ShaderType
+	{
+		VERTEX_SHADER = 1,
+		FRAGMENT_SHADER = 2,
+		GEOMETRY_SHADER = 3,
+		TESS_CONTROL_SHADER = 4,
+		TESS_EVALUATION_SHADER = 5,
+	} objShaderType;
+	Shader();
+	GLuint getShader();
+	GLuint createShaderFromFile(std::string name, ShaderType type);
+	void attachShader( GLuint program );
+	std::string loadShaderFile(const std::string fileName);
+	void getShaderLogInfo( GLuint shader );
+	void getProgramLogInfo( GLuint program );
+	void DelShader();
+	virtual ~Shader();
+};
+#endif
