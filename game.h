1203632a9c3d28734574168037a38a6f0392762e
Dark Engineer <maniodark@gmail.com>
2013-03-30 11:22:14 +0100

    Update game.h

--- game.h
+++ game.h
@@ -1,23 +1,21 @@
 #ifndef game_h
 #define game_h
 #include "includes.h"
-#include "Object.h"
+#include "Camera.h"
+#include "Texture.h"
 
 class Game
 {
 public:
 	double i;
-	GLuint vbo;
 	GLuint * crionProgram;
 	GLsizeiptr maxObjects;
-	std::vector<Object> listOfObjects;
+	virtual bool init() =0;
 	virtual void render() =0;
-	virtual void reserveSpace() =0;
 	virtual void Update() =0;
 	virtual void setWindow(int width, int height) =0;
 	virtual Game * nextGameState() =0;
-	virtual void makeBuffer();
-	virtual void makeProgram(GLuint program);
+	virtual void makeProgram(GLuint & program);
 	Game();
 	virtual ~Game();
 	
