9500b8abcaf8ac272e51670091c15c0e379482a8
Dark Engineer <maniodark@gmail.com>
2013-03-30 11:23:32 +0100

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,27 +1,19 @@
 #include "game.h"
-#include "loadTexture.h"
+#include "Texture.h"
 
 
 Game::Game()
-	:	maxObjects(100),
-		i(0)
 {}
 
 Game::~Game()
 {
 }
-void Game::makeBuffer()
-{
-	GLuint *vVbo = & vbo;
-	if( !glIsBuffer( *vVbo ) )
-		glGenBuffers( 1, vVbo);
-		glBindBuffer( GL_ARRAY_BUFFER, *vVbo );
-		
-}
 
-void Game::makeProgram(GLuint program)
+
+
+void Game::makeProgram(GLuint & program)
 {
-	Game::crionProgram = & program;
+	crionProgram = & program;
 	if( !glIsProgram(* crionProgram) )
 		* crionProgram = glCreateProgram();
 }
