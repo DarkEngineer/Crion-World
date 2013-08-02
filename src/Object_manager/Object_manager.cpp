d3168184bec2ce1087255c845b2cd8809aef9702
Dark Engineer <maniodark@gmail.com>
2013-08-02 23:21:12 +0200

    object manager changes

--- src/Object_manager/Object_manager.cpp
+++ src/Object_manager/Object_manager.cpp
@@ -11,7 +11,7 @@ Player * Object_manager::getPlayer()
 	return player;
 }
 
-std::vector <Object> Object_manager::getObjects()
+std::vector <std::unique_ptr<Object>> & Object_manager::getObjects()
 {
 	return objects;
 }
@@ -31,43 +31,38 @@ bool Object_manager::addPlayer(glm::vec3 & position, const char * sourceMesh, co
 
 bool Object_manager::addObject(const char * sourceMesh, const char * fileStructure)
 {
-	Object temp;
-	if(temp.create(sourceMesh, fileStructure))
-	{
-		objects.push_back(temp);
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	objects.push_back(std::unique_ptr<Object>(new Object(sourceMesh, fileStructure)));
+	std::cout << "LOG: Object " << sourceMesh << " created without coordinates" << std::endl; 
+
+	return true;
 }
 
 bool Object_manager::addObject(glm::vec3 & position, const char * sourceMesh, const char * fileStructure)
 {
-	Object * temp = new Object();
-	bool value = false;
-	if(temp->create(position, sourceMesh, fileStructure))
-	{
-		objects.push_back(*temp);
-		std::cout << "LOG: Object " << sourceMesh << " created" << std::endl; 
-		value = true;
-	}
-	return value;
+	objects.push_back(std::unique_ptr<Object>(new Object(position, sourceMesh, fileStructure)));
+	std::cout << "LOG: Object " << sourceMesh << " created" << std::endl; 
+
+	return true;
 }
 
 void Object_manager::render()
 {
 	if(player != 0)
 		player->render();
-	std::vector <Object>::iterator count;
+	std::vector <std::unique_ptr<Object>>::iterator count;
 	for(count = objects.begin(); count != objects.end(); count++)
-		count->render();
-	std::cout << objects.size() << std::endl;
+		(*count)->render();
+}
+
+void Object_manager::render(LightingTechnique & manager_position)
+{
+	if(player != 0)
+		
 }
 
 Object_manager::~Object_manager()
 {
 	delete player;
+	objects.clear();
 }
 
--- src/Object_manager/Object_manager.h
+++ src/Object_manager/Object_manager.h
@@ -1,23 +1,25 @@
 #ifndef Object_manager_h
 #define Object_manager_h
 #include "../Player/Player.h"
+#include "../Lighting/LightingTechnique.h"
 #include <memory>
 
 
 class Object_manager
 {
 	Player * player;
-	std::vector <Object> objects;
+	std::vector <std::unique_ptr<Object>> objects;
 
 
 public:
 	Player * getPlayer();
-	std::vector <Object> getObjects();
+	std::vector <std::unique_ptr<Object>> & getObjects();
 	bool addPlayer(const char * sourceMesh, const char * fileStructure);
 	bool addPlayer(glm::vec3 & position, const char * sourceMesh, const char * fileStructure);
 	bool addObject(const char * sourceMesh, const char * fileStructure);
 	bool addObject(glm::vec3 & position, const char * sourceMesh, const char * fileStructure);
 	virtual void render();
+	virtual void render(LightingTechnique & manager_position);
 
 	Object_manager();
 	virtual ~Object_manager();
